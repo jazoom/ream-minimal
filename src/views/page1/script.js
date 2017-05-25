3f1ee12092f2d9efcd4cf26a159dbae3d1cbfd4f
Unknown <jazoom@gmail.com>
2017-05-25 14:39:42 +1000

    remove metadata

--- src/views/page1/script.js
+++ src/views/page1/script.js
@@ -1,17 +1,5 @@
 'use strict';
 
 export default {
-  name: 'page1',
-
-  head() {
-    return {
-      title: 'page1 title',
-      meta: [
-        { vmid: 'description', name: 'description', content: 'page1 description' },
-        { vmid: 'og:title', property: 'og:title', content: 'page1 og:title' },
-        { vmid: 'og:description', property: 'og:description', content: 'page1 og:description' },
-        { vmid: 'og:image', property: 'og:image', content: 'page1 og:image' }
-      ]
-    }
-  }
+  name: 'page1'
 }
